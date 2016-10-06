422163cd25f228ec71777226da0336908d514eb8
Lupita-Franco <1715110465@utec-tgo.edu.mx>
2016-10-06 12:23:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gholamundo1
---
+++ src/hola/Hola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola;
+
+/**
+ *
+ * @author PC-4
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        
+    
+ System.out.println("hola mundo");
+    }
+}
